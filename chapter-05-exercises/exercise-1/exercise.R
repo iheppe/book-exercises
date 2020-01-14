f0f6e9884d43f90e071db4518c3562382205ee96
Isabella H <isabella.heppe@gmail.com>
2020-01-14 14:52:15 -0800

    Revert "add chapter 5 exercise answers"
    This reverts commit 2ff450dde996161f8f5efcac5c0174b42e841fe0.

--- chapter-05-exercises/exercise-1/exercise.R
+++ chapter-05-exercises/exercise-1/exercise.R
@@ -1,35 +1,26 @@
 # Exercise 1: practice with basic R syntax
 
 # Create a variable `hometown` that stores the city in which you were born
-hometown <- "Alameda"
-print(hometown)
+
 
 # Assign your name to the variable `my_name`
-my_name <- "Isabella"
-print(my_name)
+
 
 # Assign your height (in inches) to a variable `my_height`
-my_height <- 68
-print(my_height)
+
 
 # Create a variable `puppies` equal to the number of puppies you'd like to have
-puppies <- 2
-print(puppies)
+
 
 # Create a variable `puppy_price`, which is how much you think a puppy costs
-puppy_price <- 600
-print(puppy_price)
+
 
 # Create a variable `total_cost` that has the total cost of all of your puppies
-total_cost <- puppies * puppy_price
-print(total_cost)
+
 
 # Create a boolean variable `too_expensive`, set to TRUE if the cost is greater 
 # than $1,000
-too_expensive <- total_cost > 1000
-print(too_expensive)
+
 
 # Create a variable `max_puppies`, which is the number of puppies you can 
 # afford for $1,000
-max_puppies <- floor(1000 / puppy_price)
-print(max_puppies)
