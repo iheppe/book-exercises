a805f43f73ae7ef5907df54425a4fecb92571ed4
Isabella H <isabella.heppe@gmail.com>
2020-01-23 14:24:43 -0800

    fill in chapter 8 exercises 1 & 2, and chapter 10 exercise 1

--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -3,28 +3,28 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
-
+employees <- paste("Employee", 1:100)
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
-
+salary_values <- c(runif(100, 40000, 50000))
 
 # Create a vector of 100 annual salary adjustments between -5000 and 10000.
 # (A negative number represents a salary decrease due to corporate greed)
 # Again use the `runif()` function to pick 100 random numbers in that range.
-
+salary_adjustments <- c(runif(100, -5000, 10000))
 
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employees, salary_values, salary_adjustments)
 
 # Add a column to the `salaries` data frame that represents each person's
 # salary in 2018 (e.g., with the salary adjustment added in).
-
+salaries$salary_total <- salary_values + salary_adjustments
 
 # Add a column to the `salaries` data frame that has a value of `TRUE` if the 
 # person got a raise (their salary went up)
-
+salaries$got_raise <- salaries["salary_adjustments"] > 0
 
 
 ### Retrieve values from your data frame to answer the following questions
@@ -32,10 +32,10 @@
 ### cell rather than the whole row!)
 
 # What was the 2018 salary of Employee 57
-
+salaries[5, "salary_total"]
 
 # How many employees got a raise?
-
+length(salaries[salaries$got_raise == TRUE, "got_raise"])
 
 # What was the dollar value of the highest raise?
 
@@ -50,13 +50,15 @@
 
 
 # What was the average salary change?
-
+mean(salaries$salary_adjustments)
 
 # For people who did not get a raise, how much money did they lose on average?
-
+mean(salaries[salaries$got_raise == FALSE, "salary_adjustments"])
 
 ## Consider: do the above averages match what you expected them to be based on 
 ## how you generated the salaries?
 
-# Write a .csv file of your salary data to your working directory
+# Yes - they are very close to the middle of their respective bounds
 
+# Write a .csv file of your salary data to your working directory
+write.csv(salaries)
